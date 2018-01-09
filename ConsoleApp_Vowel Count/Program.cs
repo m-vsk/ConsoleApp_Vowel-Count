c31248910818e8174148e3cd14abee89dd29c46a
vsk <m-vsk@yandex.ru>
2018-01-09 22:39:32 +0300

    new code

--- ConsoleApp_Vowel Count/Program.cs
+++ ConsoleApp_Vowel Count/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp_Vowel_Count
 {
+    public static class Kata
+    {
+        public static int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+            vowelCount = str.ToLower().Count(c => vowels.Contains(c));
+            return vowelCount;// = str.Count(c => c == 'a');
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter:");
+            string sentence = Console.ReadLine().ToLower();
+            Console.WriteLine(Kata.GetVowelCount(sentence));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
+
     }
 }
---
+++ ConsoleApp_Vowel CountTests/KataTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp_Vowel_Count;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Vowel_Count.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void GetVowelCountTest()
+        {
+            Assert.AreEqual(5, Kata.GetVowelCount("abracadabra"), "Nope!");
+        }
+    }
+}
